f97c8ef2e5473b625387d5e26adafaf4d84e12cd
sakura150 <tayachelepis@gmail.com>
2025-11-13 15:59:13 +0300

    up date

--- hello_world.cpp
+++ hello_world.cpp
@@ -5,6 +5,6 @@ int main() {
     std::string name;
     std::cout << "Please enter your name: "; // Изменили текст
     std::cin >> name;
-    std::cout << "Greetings from " << name << "!" << std::endl; // Изменили приветствие
+    std::cout << "Greetings from " << name << "!" << std::endl;
     return 0;
 }
